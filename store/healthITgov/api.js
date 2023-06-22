9dda6dc7f5a5d6911be2ec52d3c6ee4adc7d8e16
Jason Kozel <jason.kozel@crowdbotics.com>
2023-06-22 15:27:29 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/healthITgov/api.js
+++ store/healthITgov/api.js
@@ -1,7 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 const healthITgov = axios.create({
-  baseURL:
-    "https://www.healthit.gov/data/open-api?source=hospital-mu-public-health-measures.csv",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
+  baseURL: "https://www.healthit.gov/data/open-api?source=hospital-mu-public-health-measures.csv",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+export const apiService = {};
