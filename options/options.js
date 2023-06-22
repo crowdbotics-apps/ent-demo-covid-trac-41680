a6caa224fea89b8668185bde3a9bf9bd8a7d1d81
Crowdbotics <team@crowdbotics.com>
2023-06-22 17:32:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-demo-covid-trac-41680",
+  url: "https://ent-demo-covid-trac-41680.botics.co",
+  api: "https://ent-demo-covid-trac-41680.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
